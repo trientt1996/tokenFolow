8f893d2d66e92ff02d49756f7469d221f77a9ee9
trien.tran <trien.tran@ekotek.vn>
2024-11-20 17:24:08 +0700

    x

--- src/components/SpotTable.js
+++ src/components/SpotTable.js
@@ -1,7 +1,8 @@
 // src/components/SpotTable.js
-import React from 'react';
+import React, { useState } from 'react';
 
 function SpotTable({ title, data, time }) {
+  console.log('dsadasd', Date.now());
   return (
     <div className="table-container">
       <h3 className="table-title">{title}</h3>
@@ -15,17 +16,28 @@ function SpotTable({ title, data, time }) {
         </thead>
         <tbody>
           {data?.map((item, index) => {
+            let textAction = 'Đứng ngoài';
+            if (
+              item?.[`macdOld${time}`] < 0 &&
+              item?.[`macd${time}`] > 0 &&
+              item?.[`trend${time}`] == 'down' &&
+              item?.[`nextTime${time}`] < Date.now()
+            ) {
+              textAction = 'Buy';
+            }
+            if (
+              item?.[`macdOld${time}`] > 0 &&
+              item?.[`macd${time}`] < 0 &&
+              item?.[`trend${time}`] == 'up' &&
+              item?.[`nextTime${time}`] < Date.now()
+            ) {
+              textAction = 'Sell';
+            }
             return (
               <tr key={index}>
                 <td className="text-blue">{item?.name}</td>
                 <td>{item?.percentChange} %</td>
-                <td>
-                  {item?.[`macdOld${time}`] < 0 && item?.[`macd${time}`] > 0
-                    ? 'Buy'
-                    : item?.[`macdOld${time}`] > 0 && item?.[`macd${time}`] < 0
-                    ? 'Sell'
-                    : 'Đứng ngoài'}
-                </td>
+                <td>{textAction}</td>
               </tr>
             );
           })}
